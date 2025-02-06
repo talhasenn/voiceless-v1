59c184dcab05598972d13cfc2e03c44def734843
Talha Sen <11887350+talhasenn@users.noreply.github.com>
2025-02-06 19:00:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8080 });
 
-let clients = {}; // Store connected clients per channel
+let clients = {};
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
-    // Handle messages from clients
+   
     console.log(`received: ${message}`);
     ws.send('Message received');
   });
